95e3f866b4a6215cc530d9ab96abb676307e981d
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-21 17:23:41 -0300

    Delete

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,9 +1,42 @@
 import { Router } from 'express'
+import slugify from 'slugify'
+import Category from '../models/Category'
 
 const adminRoutes = Router()
 
 adminRoutes.get('/categories/new', (req, res) => {
   res.render('pages/admin/categories/new')
+})
+
+adminRoutes.post('/categories/create', (req, res) => {
+  const title = req.body.title
+
+  if (title != undefined) {
+    Category.create({
+      title,
+      slug: slugify(title),
+    }).then(() => res.redirect('/'))
+  } else res.redirect('pages/admin/categories/new')
+})
+
+adminRoutes.get('/categories', (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render('pages/admin/categories/index', { categories })
+  })
+})
+
+adminRoutes.post('/categories/delete', (req, res) => {
+  const id = req.body.id
+
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id,
+        },
+      }).then(() => res.redirect('/admin/categories'))
+    } else res.redirect('/admin/categories')
+  } else res.redirect('/admin/categories')
 })
 
 export default adminRoutes
