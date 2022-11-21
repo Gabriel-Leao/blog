6e59885dd432463963890d4fc2022c699d9fdd95
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-21 18:53:24 -0300

    some tweaks

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import bodyParser from 'body-parser'
 import express from 'express'
 import routes from './routes/index.routes'
 import database from './config/db.config'
-import Article from './models/Article'
-import Category from './models/Category'
 
 const port = process.env.PORT || 3333
 const app = express()
@@ -15,22 +13,6 @@ try {
 } catch (error) {
   console.error('Unable to connect to the database:', error)
 }
-
-// try {
-//   Article.sync({ force: false }).then(() =>
-//     console.log('Table has been created successfully')
-//   )
-// } catch (error) {
-//   console.error('Unable to create table Article:', error)
-// }
-
-// try {
-//   Category.sync({ force: false }).then(() =>
-//     console.log('Table has been created successfully')
-//   )
-// } catch (error) {
-//   console.error('Unable to create table Category:', error)
-// }
 
 app.set('view engine', 'ejs')
 
