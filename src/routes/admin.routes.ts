3b5e98dc33ac5245253f0719454c6cf042bd9c9b
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-22 12:21:17 -0300

    update

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -11,12 +11,14 @@ adminRoutes.get('/categories/new', (req, res) => {
 adminRoutes.post('/categories/create', (req, res) => {
   const title = req.body.title
 
-  if (title != undefined) {
+  if (title == undefined || title == '') {
+    res.redirect('/admin/categories/new')
+  } else {
     Category.create({
       title,
       slug: slugify(title),
-    }).then(() => res.redirect('/'))
-  } else res.redirect('pages/admin/categories/new')
+    }).then(() => res.redirect('/admin/categories'))
+  }
 })
 
 adminRoutes.get('/categories', (req, res) => {
@@ -37,6 +39,39 @@ adminRoutes.post('/categories/delete', (req, res) => {
       }).then(() => res.redirect('/admin/categories'))
     } else res.redirect('/admin/categories')
   } else res.redirect('/admin/categories')
+})
+
+adminRoutes.get('/categories/edit/:id', (req, res) => {
+  const id = req.params.id
+
+  if (isNaN(+id)) {
+    res.redirect('/admin/categories')
+  } else {
+    Category.findByPk(id)
+      .then((category) => {
+        if (category != undefined) {
+          res.render('pages/admin/categories/edit', { category })
+        } else res.redirect('/admin/categories')
+      })
+      .catch((error) => {
+        res.redirect('/admin/categories')
+      })
+  }
+})
+
+adminRoutes.post('/categories/update', (req, res) => {
+  const id = req.body.id
+  const title = req.body.title
+  const slug = slugify(title)
+
+  Category.update(
+    { title, slug },
+    {
+      where: {
+        id,
+      },
+    }
+  ).then(() => res.redirect('/admin/categories'))
 })
 
 export default adminRoutes
