ae7e7dd58e55e953d18f99c78475149575023a7b
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-25 14:35:54 -0300

    filter by category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express'
+import Article from '../models/Article'
+import Category from '../models/Category'
 
 const categoriesRoutes = Router()
 
-categoriesRoutes.get('/', (req, res) => {
-  res.send('ROTA DE CATEGORIAS')
+categoriesRoutes.get('/:slug', (req, res) => {
+  const slug = req.params.slug
+  Category.findOne({ where: { slug }, include: [{ model: Article }] })
+    .then((category: any) => {
+      if (category == undefined) {
+        res.redirect('/')
+      } else {
+        Category.findAll().then((categories) => {
+          res.render('pages/home', { articles: category.articles, categories })
+        })
+      }
+    })
+    .catch((error) => res.redirect('/'))
 })
 
 export default categoriesRoutes
