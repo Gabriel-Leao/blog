922fa13be60c6ee57ebd6211a65ffb49560b22db
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-28 15:38:02 -0300

    pagination

--- src/routes/articles.routes.ts
+++ src/routes/articles.routes.ts
@@ -20,4 +20,34 @@ articlesRoutes.get('/:slug', (req, res) => {
     .catch((error) => res.redirect('/'))
 })
 
+articlesRoutes.get('/page/:page', (req, res) => {
+  const page = req.params.page
+  let offset: number
+  let next = false
+
+  if (isNaN(+page) || +page == 1) {
+    offset = 0
+  } else offset = (+page - 1) * 4
+
+  Article.findAndCountAll({ order: [['id', 'desc']], limit: 4, offset }).then(
+    (articles) => {
+      if (offset + 4 < articles.count) next = true
+
+      let maxPages = articles.count / 4
+      if (articles.count % 4 != 0) maxPages++
+
+      const result = {
+        page: +page,
+        maxPages,
+        next,
+        articles,
+      }
+
+      Category.findAll().then((categories) => {
+        res.render('pages/article/page', { result, categories })
+      })
+    }
+  )
+})
+
 export default articlesRoutes
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -8,9 +8,22 @@ import categoriesRoutes from './categories.routes'
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  Article.findAll({ order: [['id', 'desc']] }).then((articles) => {
+  Article.findAndCountAll({
+    order: [['id', 'desc']],
+    limit: 4,
+    offset: 0,
+  }).then((articles) => {
+    let maxPages = articles.count / 4
+    if (articles.count % 4 != 0) maxPages++
+
+    const result = {
+      page: 1,
+      maxPages,
+      next: true,
+      articles,
+    }
     Category.findAll().then((categories) => {
-      res.render('pages/home', { articles, categories })
+      res.render('pages/home', { categories, result })
     })
   })
 })
