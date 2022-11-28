51d08e9f5d6fd2a0c0a6a8ba2b8d383e1fb26c99
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-28 20:21:11 -0300

    User edit

--- src/routes/admin/user.routes.ts
+++ src/routes/admin/user.routes.ts
@@ -5,7 +5,9 @@ import bcrypt from 'bcryptjs'
 const userRoutes = Router()
 
 userRoutes.get('/', (req, res) => {
-  res.render('pages/admin/users/index')
+  User.findAll().then((users) => {
+    res.render('pages/admin/users/index', { users })
+  })
 })
 
 userRoutes.get('/new', (req, res) => {
@@ -36,6 +38,82 @@ userRoutes.post('/create', (req, res) => {
       }
     })
   }
+})
+
+userRoutes.get('/edit/:id', (req, res) => {
+  const id = req.params.id
+
+  if (isNaN(+id)) {
+    res.redirect('/admin/users/')
+  } else {
+    User.findByPk(id).then((user) => {
+      res.render('pages/admin/users/edit', { user })
+    })
+  }
+})
+
+interface user {
+  id: number
+  email: string
+  password: string
+}
+
+userRoutes.post('/update', (req, res) => {
+  const id = req.body.id
+  const password = req.body.userPassword
+  const email = req.body.userEmail
+
+  if (email == '') {
+    res.redirect(`/admin/users/edit/${id}`)
+  } else {
+    User.findByPk(id).then((user: any) => {
+      if (user.email == email) {
+        if (password == '') {
+          res.redirect(`/admin/users/edit/${id}`)
+        } else {
+          try {
+            const salt = bcrypt.genSaltSync(10)
+            const hash = bcrypt.hashSync(password, salt)
+            User.update({ password: hash }, { where: { id } }).then(() => {
+              res.redirect('/admin/users')
+            })
+          } catch (error) {
+            res.redirect(`/admin/users/edit/${id}`)
+          }
+        }
+      } else {
+        User.findOne({ where: { email } }).then((user) => {
+          if (user == undefined) {
+            if (password == '') {
+              try {
+                User.update({ email }, { where: { id } }).then(() => {
+                  res.redirect('/admin/users')
+                })
+              } catch (error) {
+                res.redirect(`/admin/users/edit/${id}`)
+              }
+            } else {
+              try {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.hashSync(password, salt)
+                User.update({ email, password: hash }, { where: { id } }).then(
+                  () => {
+                    res.redirect('/admin/users')
+                  }
+                )
+              } catch (error) {
+                res.redirect(`/admin/users/edit/${id}`)
+              }
+            }
+          } else res.redirect(`/admin/users/edit/${id}`)
+        })
+      }
+    })
+  }
+})
+
+userRoutes.post('/delete', (req, res) => {
+  res.render('pages/admin/users/new')
 })
 
 export default userRoutes
