667de74df05466874e08eb6797eb32d0a8f6e66e
Gabriel-Leao <gabriel.leao2507@gmail.com>
2023-02-25 16:47:24 -0300

    fix: ajuste na rota de categorias para mostrar apenas os artigos que sao daquela categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -11,8 +11,25 @@ categoriesRoutes.get('/:slug', (req, res) => {
       if (category == undefined) {
         res.redirect('/')
       } else {
-        Category.findAll().then((categories) => {
-          res.render('pages/home', { articles: category.articles, categories })
+        Article.findAndCountAll({
+          where: { categoryId: category.id },
+          order: [['id', 'desc']],
+          limit: 4,
+          offset: 0,
+        }).then((articles) => {
+          let maxPages = articles.count / 4
+          if (articles.count % 4 != 0) maxPages++
+
+          const result = {
+            page: 1,
+            maxPages,
+            next: true,
+            articles,
+          }
+
+          Category.findAll().then((categories) => {
+            res.render('pages/home', { categories, result })
+          })
         })
       }
     })
